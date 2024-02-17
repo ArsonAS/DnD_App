774ec62e6a45b0de62cca581207e62302b04411b
Arson <binauralblues@gmail.com>
2024-02-17 18:22:51 -0500

    Ajout de dépendances Maven dans POM.

--- backend/src/main/java/com/dnd_app/Main.java
+++ backend/src/main/java/com/dnd_app/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.dnd_app;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
