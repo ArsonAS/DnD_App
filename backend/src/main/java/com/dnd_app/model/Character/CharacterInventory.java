d33e04a4da73c07b583aa45102d1dfc1465d0d73
Arson <binauralblues@gmail.com>
2024-03-02 15:26:07 -0500

    Propriétés de la classe CharacterSkills et CharacterInventory

--- backend/src/main/java/com/dnd_app/model/Character/CharacterInventory.java
+++ backend/src/main/java/com/dnd_app/model/Character/CharacterInventory.java
@@ -1,8 +1,13 @@
 package com.dnd_app.model.Character;
 
 import jakarta.persistence.Embeddable;
-
+//TODO: Rechercher comment implementer le equipe du personnage
 @Embeddable
 public class CharacterInventory {
-
+    private String equipment;
+    private int copperPieces;
+    private int silverPieces;
+    private int electrumPieces;
+    private int goldPieces;
+    private int platinumPieces;
 }
