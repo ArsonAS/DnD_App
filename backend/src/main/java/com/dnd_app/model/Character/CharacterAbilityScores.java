946a9ac5dd7966670895e1d2dd25bd878375a829
Arson <binauralblues@gmail.com>
2024-03-02 14:52:24 -0500

    Propriétés de la classe CharacterAbilityScore

--- backend/src/main/java/com/dnd_app/model/Character/CharacterAbilityScores.java
+++ backend/src/main/java/com/dnd_app/model/Character/CharacterAbilityScores.java
@@ -4,4 +4,10 @@ import jakarta.persistence.Embeddable;
 
 @Embeddable
 public class CharacterAbilityScores {
+    private int strength;
+    private int dexterity;
+    private int constitution;
+    private int intelligence;
+    private int wisdom;
+    private int charisma;
 }
