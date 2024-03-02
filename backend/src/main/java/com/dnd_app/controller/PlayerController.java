127432c83698a4ecb8caadcf4a8d020eeae298ed
Arson <binauralblues@gmail.com>
2024-03-02 16:43:00 -0500

    Creation du clase PlayerController

--- backend/src/main/java/com/dnd_app/controller/PlayerController.java
+++ backend/src/main/java/com/dnd_app/controller/PlayerController.java
@@ -1,2 +1,27 @@
-package com.dnd_app.controller;public class PlayerController {
+package com.dnd_app.controller;
+
+import com.dnd_app.dto.PlayerDTO;
+import com.dnd_app.service.PlayerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("api/players")
+public class PlayerController {
+    private PlayerService playerService;
+
+
+    public PlayerController(PlayerService playerService) {
+        this.playerService = playerService;
+    }
+
+    public ResponseEntity<PlayerDTO> createPlayer(@RequestBody PlayerDTO playerDTO){
+        return playerService.createPlayer(playerDTO)
+                .map(player -> ResponseEntity.ok(playerDTO))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+
 }
--- backend/src/main/java/com/dnd_app/repository/PlayerRepository.java
+++ backend/src/main/java/com/dnd_app/repository/PlayerRepository.java
@@ -3,5 +3,10 @@ package com.dnd_app.repository;
 import com.dnd_app.model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PlayerRepository extends JpaRepository<Player, Long> {
+    Optional<Player> findByEmail(String email);
+
+    Optional<Player> findByUsername(String username);
 }
