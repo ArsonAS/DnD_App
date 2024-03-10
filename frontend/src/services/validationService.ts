4668bc53bae75e4452021464f09a96927ecd6a5a
Arson <binauralblues@gmail.com>
2024-03-10 15:46:53 -0400

    Corrections du validation des mots de passe

--- frontend/src/services/validationService.ts
+++ frontend/src/services/validationService.ts
@@ -1,10 +1,8 @@
 export const validateEmail = (email: string): boolean => {
     return (
-        email !== "" &&
-        /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/i.test(email) &&
-        (email.endsWith("gmail.com") ||
-            email.endsWith("hotmail.com") ||
-            email.endsWith("outlook.com"))
+        email !== ""
+        && /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/i.test(email)
+        && email.endsWith(".com")
     );
 };
 
@@ -12,22 +10,12 @@ export const validatePassword = (password: string): boolean => {
     const minLength = 8;
     const hasUpperCase = /[A-Z]/.test(password);
     const hasLowerCase = /[a-z]/.test(password);
-    //const hasDigit = /[0-9]/.test(password);
-    //const hasSpecialChar = /[!@#$%^&*()_+{}[\]:;<>,.?~\\-]/.test(password);
+    const hasDigit = /[0-9]/.test(password);
 
-    return (
-        password.length >= minLength &&
-        hasUpperCase &&
-        hasLowerCase //&&
-        //hasDigit &&
-        //hasSpecialChar
-    );
+    return (password.length >= minLength && hasUpperCase && hasLowerCase && hasDigit);
 };
 
-export const validatePasswordConfirmation = (
-    password: string,
-    passwordConfirmation: string
-): boolean => {
+export const validatePasswordConfirmation = (password: string, passwordConfirmation: string): boolean => {
     return password === passwordConfirmation;
 };
 
