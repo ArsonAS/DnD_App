1272007c6a213a9f14876e8bfed362dbbae9d3c4
Arson <binauralblues@gmail.com>
2024-03-16 17:46:13 -0400

    Methodes pour la creation des campaignes et de characteres

--- backend/src/main/java/com/dnd_app/service/ClientService.java
+++ backend/src/main/java/com/dnd_app/service/ClientService.java
@@ -1,8 +1,10 @@
 package com.dnd_app.service;
 
+import com.dnd_app.dto.CharacterDTO;
 import com.dnd_app.dto.ClientDTO;
 import com.dnd_app.model.Salt;
 import com.dnd_app.model.Client;
+import com.dnd_app.repository.CharacterRepository;
 import com.dnd_app.repository.SaltRepository;
 import com.dnd_app.repository.ClientRepository;
 import jakarta.transaction.Transactional;
@@ -16,10 +18,13 @@ import java.util.Optional;
 public class ClientService {
     private final ClientRepository clientRepository;
     private final SaltRepository saltRepository;
+    private final CharacterRepository characterRepository;
 
-    public ClientService(ClientRepository clientRepository, SaltRepository saltRepository) {
+    public ClientService(ClientRepository clientRepository, SaltRepository saltRepository,
+                         CharacterRepository characterRepository) {
         this.clientRepository = clientRepository;
         this.saltRepository = saltRepository;
+        this.characterRepository = characterRepository;
     }
 
 
@@ -35,8 +40,8 @@ public class ClientService {
     public Optional<ClientDTO> createClient(ClientDTO clientDTO){
         if (clientDTO == null) throw new IllegalArgumentException("Client cannot be null");
 
-        Optional<Client> userOptional = clientRepository.findByUsername(clientDTO.getUsername());
-        if (userOptional.isPresent()) throw new IllegalArgumentException("Client already exists");
+        Optional<Client> clientOptional = clientRepository.findByUsername(clientDTO.getUsername());
+        if (clientOptional.isPresent()) throw new IllegalArgumentException("Client already exists");
 
         Client newClient = clientDTO.fromDTO();
         hashAndSaltPassword(newClient);
@@ -46,5 +51,16 @@ public class ClientService {
     public Optional<ClientDTO> findClientById(Long clientId){
         return Optional.of(new ClientDTO(clientRepository.findById(clientId).orElseThrow(() -> new NoSuchElementException("Client not found"))));
     }
+
+    public Optional<CharacterDTO> createCharacter (CharacterDTO characterDTO){
+        if(characterDTO == null) throw new IllegalArgumentException("Character cannot be null");
+        return Optional.of(new CharacterDTO(characterRepository.save(characterDTO.fromDTO())));
+    }
+
+    public Optional<CharacterDTO> findCharacterById(Long characterId){
+        return Optional.of(new CharacterDTO(characterRepository.findById(characterId).orElseThrow(() -> new NoSuchElementException("Character not found"))));
+    }
+
+
 
 }
