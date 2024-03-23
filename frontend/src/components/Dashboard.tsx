c119ec08815a5eda82d4a4ccc1743ad71c169407
Arson <binauralblues@gmail.com>
2024-03-23 16:48:58 -0400

    service pour creer un charactere

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -4,6 +4,8 @@ import {Link, useLocation, useNavigate, useParams} from "react-router-dom";
 import {Button, ButtonGroup, Col, Container, Form, FormControl, Row} from "react-bootstrap";
 import {getClientById} from "../services/clientService";
 import {getClientId, logout} from "../security/authService";
+import {NavBar} from "./NavBar";
+import {CHARACTER_PREFIX} from "../constants/apiPrefixes";
 
 export const Dashboard = () => {
     const [client, setClient] = useState<Client>();
@@ -25,24 +27,41 @@ export const Dashboard = () => {
         navigate("/");
     };
 
+    const goToCreateCharacter = () => {
+        navigate("/api/clients/newCharacter");
+    }
 
     return (
         <Container fluid className="p-2 bg-dark vh-100 text-warning">
-            <h2>{client?.username}</h2>
-            <div className="float-right">
-                {client?.role === "DM"
-                    ?
-                        <ButtonGroup>
-                            <Button>Créer une campaign</Button>
-                            <Button>Créer un PNJ</Button>
-                        </ButtonGroup>
-                    :
-                        <ButtonGroup>
-                            <Button>Créer un personnage</Button>
-                        </ButtonGroup>
-                }
-                <Button variant="outline-warning" onClick={disconnect} className="me-2">Se déconnecter</Button>
-            </div>
+            <Row className="px-3 fixed-top mb-5 py-2">
+                <Col sm={{span: 3, offset: 0}} className="bg-dark px-2 py-1 align-self-sm-start">
+                    <h2>{client?.username}</h2>
+                </Col>
+                <Col sm={{span: 6, offset: 0}} className="bg-dark px-2 py-1">
+                    <Row className="px-2 flex-row justify-content-around">
+                        {client?.role === "DM"
+                            ?
+                            <ButtonGroup>
+                                <Button variant="warning">Créer une campaign</Button>
+                                <Button>Créer un PNJ</Button>
+                            </ButtonGroup>
+                            :
+                            <ButtonGroup>
+                                <Button variant="warning" onClick={goToCreateCharacter}>Créer un personnage</Button>
+
+                            </ButtonGroup>
+                        }
+                    </Row>
+                </Col>
+                <Col sm={{span: 3, offset: 0}} className="bg-dark align-self-sm-end">
+                    <NavBar/>
+                </Col>
+
+
+            </Row>
+
+
+
         </Container>
     );
 
--- frontend/src/services/clientService.ts
+++ frontend/src/services/clientService.ts
@@ -9,6 +9,10 @@ export const getClientById = async (id: number): Promise<AxiosResponse<Client>> 
     return http.get<Client>(`${CLIENT_PREFIX}/${id}`);
 };
 
+
+export const createCharacter = async (character: Character) => {
+    return http.post(CHARACTER_PREFIX, character);
+}
 export const getCharacterById = async (charId: number): Promise<AxiosResponse<Character>> => {
     return http.get<Character>(CHARACTER_PREFIX, {
         params:{
