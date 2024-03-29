098b27a6744b395fdfdde96e7e12f96240212525
Arson <binauralblues@gmail.com>
2024-03-29 14:25:35 -0400

    Ajoute de la liste des characteres au tableau du bord

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -2,30 +2,36 @@ import React, {useEffect, useState} from "react";
 import {Client} from "../models/Client";
 import {Link, useLocation, useNavigate, useParams} from "react-router-dom";
 import {Button, ButtonGroup, Col, Container, Form, FormControl, Row} from "react-bootstrap";
-import {getClientById} from "../services/clientService";
+import {getAllCharactersByClientId, getClientById} from "../services/clientService";
 import {getClientId, logout} from "../security/authService";
 import {NavBar} from "./NavBar";
-import {CHARACTER_PREFIX} from "../constants/apiPrefixes";
+import {Character} from "../models/Character";
+import {CharacterList} from "./CharacterList";
 
 export const Dashboard = () => {
     const [client, setClient] = useState<Client>();
+    const [characters, setCharacters] = useState<Character[]>([]);
     const navigate = useNavigate();
     const location = useLocation();
     const params = useParams();
 
     useEffect(() => {
         if (client !== undefined) return;
-
         const id = getClientId()
 
         getClientById(parseInt(id!)).then((response) => {
             setClient(response.data);
         })
-    });
-    const disconnect = () => {
-        logout();
-        navigate("/");
-    };
+
+    }, [client]);
+
+    useEffect(() => {
+        if (client === undefined) return;
+
+        getAllCharactersByClientId(client.id!).then((response) => {
+            setCharacters(response.data);
+        })
+    }, [client, setCharacters]);
 
     const goToCreateCharacter = () => {
         navigate("/character/new");
@@ -33,11 +39,13 @@ export const Dashboard = () => {
 
     return (
         <Container fluid className="p-2 bg-dark vh-100 text-warning">
-            <Row className="px-3 fixed-top mb-5 py-2">
-                <Col sm={{span: 3, offset: 0}} className="bg-dark px-2 py-1 align-self-sm-start">
-                    <h2>{client?.username}</h2>
+            <Row className="px-3 sticky-top mb-5 py-2 border border-bottom-2 border-left-0 border-right-0 border-warning align-items-baseline">
+                <Col sm={{span: 3, offset: 0}} className="bg-dark px-2 py-1 align-items-baseline">
+                    <p>
+                        <span className="h3">{client?.username}</span> : <span className="h5">{client?.role}</span>
+                    </p>
                 </Col>
-                <Col sm={{span: 6, offset: 0}} className="bg-dark px-2 py-1">
+                <Col sm={{span: 5, offset: 0}} className="bg-dark px-2 py-1">
                     <Row className="px-2 flex-row justify-content-around">
                         {client?.role === "DM"
                             ?
@@ -48,17 +56,17 @@ export const Dashboard = () => {
                             :
                             <ButtonGroup>
                                 <Button variant="warning" onClick={goToCreateCharacter}>Créer un personnage</Button>
-
                             </ButtonGroup>
                         }
                     </Row>
                 </Col>
-                <Col sm={{span: 3, offset: 0}} className="bg-dark align-self-sm-end">
+                <Col sm={{span: 3, offset: 0}} className="bg-dark align-self-end">
                     <NavBar/>
                 </Col>
-
-
             </Row>
+            <Col sm={{span: 5, offset: 0}}>
+                <CharacterList characters={characters}/>
+            </Col>
 
 
 
