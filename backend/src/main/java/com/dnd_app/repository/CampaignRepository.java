06c998bea0d8f41bc5f284fd5e2f8ef8cc93e043
Arson <binauralblues@gmail.com>
2024-04-06 14:41:42 -0400

    Modifications dans les classes CampaignRepository et Character Repository

--- backend/src/main/java/com/dnd_app/repository/CampaignRepository.java
+++ backend/src/main/java/com/dnd_app/repository/CampaignRepository.java
@@ -4,10 +4,13 @@ import com.dnd_app.model.Campaign;
 import com.dnd_app.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CampaignRepository extends JpaRepository<Campaign, Long> {
 
     Optional<Campaign> findByName(String name);
+    List<Campaign> findAllByCharactersId(Long characterId);
+    List<Campaign> findAllByClient(Client client);
 
 }
--- backend/src/main/java/com/dnd_app/repository/CharacterRepository.java
+++ backend/src/main/java/com/dnd_app/repository/CharacterRepository.java
@@ -8,4 +8,5 @@ import java.util.List;
 
 public interface CharacterRepository extends JpaRepository<Character, Long> {
     List<Character> findAllByClient(Client client);
+    List<Character> findAllByCampaignsId(Long campaignId);
 }
