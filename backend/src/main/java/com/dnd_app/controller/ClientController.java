119e06cf5893ca57d82c7d973db956e3088cc9f9
Arson <binauralblues@gmail.com>
2024-04-06 14:49:13 -0400

    Ajout des metodes pour la gestion des campagnes dans Controlleur

--- backend/src/main/java/com/dnd_app/controller/ClientController.java
+++ backend/src/main/java/com/dnd_app/controller/ClientController.java
@@ -1,5 +1,6 @@
 package com.dnd_app.controller;
 
+import com.dnd_app.dto.CampaignDTO;
 import com.dnd_app.dto.CharacterDTO;
 import com.dnd_app.dto.ClientDTO;
 import com.dnd_app.service.ClientService;
@@ -17,14 +18,12 @@ import java.util.List;
 public class ClientController {
     private final ClientService clientService;
 
-
     @GetMapping("/{id}")
     public ResponseEntity<ClientDTO> getClientById(@PathVariable Long id){
         return clientService.findClientById(id)
                 .map(client -> ResponseEntity.ok(client))
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
-
     @PutMapping("/{id}")
     public ResponseEntity<ClientDTO> updateClientById(@RequestParam("clientId") Long clientId){
         return clientService.updateRoleById(clientId)
@@ -38,18 +37,37 @@ public class ClientController {
                 .map(character -> new ResponseEntity<HttpStatus>(HttpStatus.CREATED))
                 .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
-
     @GetMapping("/character")
     public ResponseEntity<CharacterDTO> getCharacterById(@RequestParam("charId") Long charId){
         return clientService.findCharacterById(charId)
                 .map(character -> ResponseEntity.ok(character))
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
-
     @GetMapping("/characters")
     public ResponseEntity<List<CharacterDTO>> getAllCharactersByClientId(@RequestParam("clientId") Long clientId){
         return ResponseEntity.ok(clientService.findAllCharactersByClientId(clientId));
     }
+
+
+    @PostMapping("/campaigns")
+    public ResponseEntity<HttpStatus> createCampaign(@RequestBody CampaignDTO campaignDTO, @RequestParam("clientId") Long clientId){
+        return clientService.createCampaign(campaignDTO, clientId)
+                .map(campaign -> new ResponseEntity<HttpStatus>(HttpStatus.CREATED))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+    @GetMapping("/campaign")
+    public ResponseEntity<CampaignDTO> getCampaignById(@RequestParam("campaignId") Long campaignId){
+        return clientService.findCampaignById(campaignId)
+                .map(campaign -> ResponseEntity.ok(campaign))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+    @GetMapping("/campaigns")
+    public ResponseEntity<List<CampaignDTO>> getAllCampaignsByClientId(@RequestParam("clientId") Long clientId){
+        return ResponseEntity.ok(clientService.findAllCampaignsByClientId(clientId));
+    }
+
+
+
 
 
 
